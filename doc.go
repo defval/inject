844b0829a3047572ecde1e73236403fb7d24df29
Maxim Bovtunov <dev@defval.com>
2019-05-29 14:34:49 +0300

    Add doc.go

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Dependency injection container allows you to inject dependencies
+// into constructors or structures without the need to have specified
+// each argument manually.
+package inject // import "github.com/defval/inject"
